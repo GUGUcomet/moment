a57607f1076ccfa60650660751a814c2279214d3
GUGUcomet <83127928+GUGUcomet@users.noreply.github.com>
2022-08-26 23:41:40 +0900

    tailwind css 추가

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./js/*.{html,js}",
+    "./html/*.{html,js}",
+    "./index.html",
+    "./intro.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
